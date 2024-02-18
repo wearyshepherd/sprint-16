25ff67788b5f85e92ace8927e92c53eea8d43cb5
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2024-02-17 19:43:41 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  const generatePokemon = () => {
+    const pokemonArray = [];
+    for (let i = 0; i < 6; i++) {
+      fetchRandomPokemon(i + 1).then(data => {
+        pokemonArray.push(data);
+        if (pokemonArray.length === 6) {
+          setPokemonData(pokemonArray);
+        }
+      });
+    }
+  };
+
+  const fetchRandomPokemon = async (pokemonNumber) => {
+    const pokemonId = Math.floor(Math.random() * 151) + 1;
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    const data = await response.json();
+    return {
+      id: pokemonNumber,
+      name: capitalizeFirstLetter(data.name),
+      image: data.sprites.front_default
+    };
+  };
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  return (
+    <div className="App">
+      <div className="transparent-container">
+        <h1>Pokémon Generator</h1>
+        <button id="generateButton" onClick={generatePokemon}>Generate Pokémon</button>
+        <div className="pokemon-container">
+          {pokemonData.map(pokemon => (
+            <div className="pokemon" key={pokemon.id}>
+              <img src={pokemon.image} alt={pokemon.name} />
+              <p>{pokemon.name}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <footer className="App-footer">
+        Created by Trippleten Student Carlos Chavez
+      </footer>
+    </div>
+  );
+}
+
+export default App;
