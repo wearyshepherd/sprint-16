448c4e839b935c0a642a4c02d274742941a37bf5
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2024-02-18 22:59:02 -0800

    Update About.js

--- src/About.js
+++ src/About.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
-      <p>This page contains information about the project and its creator.</p>
+      <h1>About Me</h1>
+      <p>This is some information about Carlos Chavez.</p>
+      <Link to="/">Go Back to Home</Link> {/* This line adds a link to the home page */}
+      {/* More about content */}
     </div>
   );
 };
