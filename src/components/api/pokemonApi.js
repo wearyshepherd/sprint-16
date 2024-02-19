3ecfaecc0b8f0f703d48a8b850519357628c5b7b
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2024-02-19 01:31:48 -0800

    newy

--- src/components/api/pokemonApi.js
+++ src/components/api/pokemonApi.js
@@ -12,3 +12,5 @@ const fetchRandomPokemon = async (pokemonNumber) => {
 const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
+
+export { fetchRandomPokemon, capitalizeFirstLetter };
