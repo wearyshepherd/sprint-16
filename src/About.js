807d9fdd69cef7ab9f4c3bb41ec5c81a86bc46ee
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2024-02-18 21:18:34 -0800

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import backgroundImage from './images/pokemon-background.png';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is some information about Carlos Chavez.</p>
+      {/* More about content */}
+    </div>
+  );
+};
+
+export default About;
