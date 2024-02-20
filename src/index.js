54bfea08e133afccb146374791dafe6ec40e06e3
Carlos Chavez <133473576+wearyshepherd@users.noreply.github.com>
2024-02-19 22:21:33 -0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import About from './components/About/About';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './fonts/fonts.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
